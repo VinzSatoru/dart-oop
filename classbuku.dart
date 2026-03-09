ea0bd93e806db43e211b27be793e3f3a00eefc3e
VinzSatoru <ahmadnoviandz05@gmail.com>
2026-03-09 20:28:19 +0700

    menambahkan tahun terbit dan penerbit buku

--- classbuku.dart
+++ classbuku.dart
@@ -1,18 +1,22 @@
 class Buku {
   String judul;
   String pengarang;
+  int tahunTerbit;
+  String penerbit;
 
   // Parameterized Constructor
-  Buku(this.judul, this.pengarang);
+  Buku(this.judul, this.pengarang, this.tahunTerbit, this.penerbit);
 
   void tampilkanInfo() {
-    print("Judul: $judul, Pengarang: $pengarang");
+    print(
+      "Judul: $judul, Pengarang: $pengarang, Tahun Terbit: $tahunTerbit, Penerbit: $penerbit",
+    );
   }
 }
 
 void main() {
   // Membuat objek dengan memberikan nilai pada konstruktor
-  var buku1 = Buku("The Art of Stoicism", "Ian Tuhovsky");
+  var buku1 = Buku("The Art of Stoicism", "Ian Tuhovsky", 2022, "Gramedia");
 
   print("--- Informasi Buku ---");
   buku1.tampilkanInfo();
